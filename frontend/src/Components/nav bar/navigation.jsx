f0664ae66c4f9542d8d7ce904816f373d462e12c
unknown <Atsedema004@gmail.com>
2024-07-23 14:11:48 +0300

    adding sidebar

--- frontend/src/Components/nav bar/navigation.jsx
+++ frontend/src/Components/nav bar/navigation.jsx
@@ -1,9 +1,11 @@
 import './navigation.css';
 import { FaUserCircle } from 'react-icons/fa';
 import logo from '../../assets/Mk-logo.jpg'; 
+import Sidebar from '../sidebar/sidebar';
 const NavBar = () => {
   return (
     <nav className="navbar">
+      <Sidebar/>
       <img src={logo} alt="Logo" className="logo" />
       <input type="text" placeholder="Search" className="search-bar" />
       <div className="profile-icon">
