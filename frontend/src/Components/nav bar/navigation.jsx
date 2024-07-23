b1172c525a7b3e1ff59b3c0016f1c470311aaddd
Nebagit <144024909+Nebagit@users.noreply.github.com>
2024-07-23 05:31:44 -0400

    Updated profile pop up

--- frontend/src/Components/nav bar/navigation.jsx
+++ frontend/src/Components/nav bar/navigation.jsx
@@ -1,14 +1,24 @@
+import  { useState } from 'react';
 import './navigation.css';
 import { FaUserCircle } from 'react-icons/fa';
-import logo from '../../assets/Mk-logo.jpg'; 
+import logo from '../../assets/Mk-logo.jpg';
+import ProfilePopup from './profilepopup';
+
 const NavBar = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleProfileClick = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${showPopup ? 'blurred' : ''}`}>
       <img src={logo} alt="Logo" className="logo" />
       <input type="text" placeholder="Search" className="search-bar" />
-      <div className="profile-icon">
+      <div className="profile-icon" onClick={handleProfileClick}>
         <FaUserCircle className="icon" />
       </div>
+      {showPopup && <ProfilePopup onClose={() => setShowPopup(false)} />}
     </nav>
   );
 };
