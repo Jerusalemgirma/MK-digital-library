a47caeb5a627ec665beb9e3b2e448069354545a0
NMBY21 <betelihemmengist21@gmail.com>
2024-07-29 12:59:14 +0300

    manage account

---
+++ frontend/src/pages/Manage Account/ManageAccount.jsx
@@ -0,0 +1,15 @@
+// src/pages/ManageAccount.jsx
+import React from 'react';
+import './ManageAccount.css'; // Optional, for styling
+
+const ManageAccount = () => {
+  return (
+    <div className="manage-account-page">
+      <h1>Manage Your Account</h1>
+      <p>Here you can update your account information.</p>
+      {/* Add form or details for managing the account */}
+    </div>
+  );
+};
+
+export default ManageAccount;
