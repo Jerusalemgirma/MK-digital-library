b9b7814cb1cac054baf2bd6686534b11a3a1d98b
Nebiyu Tefera <144024909+Nebagit@users.noreply.github.com>
2024-08-02 07:16:30 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,39 @@
-import './App.css';
-import React from 'react';
-import NavBar from './components/nav bar/navigation'; // Ensure this path is correct
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Components/pages/Home';
-import ManageAccount from './Components/pages/Manage Account/ManageAccount'; // Import ManageAccount page
+  import './App.css';
+  import NavBar from './Components/nav bar/navigation';
+  import SideBar from './Components/Sidebar/side';
+  import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+  import Home from './Components/Pages/Home/home';
+  import Ebook from './Components/Pages/Ebook/ebook';
+  import Audio from './Components/Pages/Audio/audio';
+  import Video from './Components/Pages/Video/video';
+  import Magazines from './Components/Pages/Magazines/magazines';
+  import MyActivity from './Components/Pages/MyActivity/myactivity';
+  import History from './Components/Pages/History/history';
+  import Categories from './Components/Pages/Categories/categories';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/manageaccount" element={<ManageAccount />} /> 
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
 
-export default App;
+  function App() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="main-content">
+            <SideBar />
+            <div className="page-content">
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/ebook" element={<Ebook />} />
+                <Route path="/audio" element={<Audio />} />
+                <Route path="/video" element={<Video />} />
+                <Route path="/magazines" element={<Magazines />} />
+                <Route path="/myactivity" element={<MyActivity />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/categories" element={<Categories />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>)
+  }
+
+  export default App;
